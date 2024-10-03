63b47262651e04514d7c4615a2f8be06faff7605
DmemBRU <danielrodrigo2004@gmail.com>
2024-10-03 21:24:04 +0200

    sdfsdfxdfxdfx

--- Assets/scripts/bola.cs
+++ Assets/scripts/bola.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class bola : MonoBehaviour
 {
+    Rigidbody rb;
     [SerializeField] int velocidad;
+    [SerializeField]Vector3 direccionSalto;
+    [SerializeField] int fuerzasalto;
     Vector3 direccion;
+    [SerializeField] int fuerza;
     // Start is called before the first frame update
     void Start()
     {
-        
+       rb =GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
@@ -18,8 +22,13 @@ public class bola : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
+
         direccion.z = h;
         direccion.x = v;
-        transform.Translate(direccion * velocidad * Time.deltaTime);
+        rb.AddForce(direccion * fuerza, ForceMode.Force);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(direccionSalto * fuerzasalto, ForceMode.Impulse);
+        }
     }
 }
