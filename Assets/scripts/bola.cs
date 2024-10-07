4b79ac90dfb1a65669758234e8cc4c498b4ebb2f
DmemBRU <danielrodrigo2004@gmail.com>
2024-10-07 17:29:32 +0200

    ftyfthfth

--- Assets/scripts/bola.cs
+++ Assets/scripts/bola.cs
@@ -10,6 +10,7 @@ public class bola : MonoBehaviour
     [SerializeField] int fuerzasalto;
     Vector3 direccion;
     [SerializeField] int fuerza;
+    [SerializeField] int vida;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,16 +20,35 @@ public class bola : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal") * -1;
+        float v = Input.GetAxis("Vertical") ;
 
 
         direccion.z = h;
         direccion.x = v;
-        rb.AddForce(direccion * fuerza, ForceMode.Force);
+        rb.AddForce(direccion.normalized * fuerza, ForceMode.Force);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<Rigidbody>().AddForce(direccionSalto * fuerzasalto, ForceMode.Impulse);
+        }
+        //Existe el FixedUpdate que es un ciclo de fisicas el cual es constante se reproduce cada 0.02 segundos SIEMPRE
+        
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "coleccionable")
+        {
+            Destroy(other.gameObject);
+        }
+        if(other.gameObject.CompareTag("trampa"))
+        {
+            Destroy(other.gameObject);
+            vida -= 10;
+        }
+        if (vida <= 0)
+        {
+            Destroy(this.gameObject);
         }
     }
 }
